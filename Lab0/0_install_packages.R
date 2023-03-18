9817e3df9fd4559fb07988744c14ddce1283c3de
sbastide <sbastide@uchicago.edu>
2023-03-18 17:21:38 -0500

    update Lab0

--- Lab0/0_install_packages.R
+++ Lab0/0_install_packages.R
@@ -1,6 +1,7 @@
 # This script contains the code to install part of the packages that we will need for the next labs.
 install.packages("BiocManager")
 install.packages("tidyverse")
+install.packages("ggrepel")
 install.packages("data.table")
 install.packages("reshape2")
 install.packages("Seurat")
